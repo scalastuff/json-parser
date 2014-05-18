ab08ef1b0a741d8176b44c78f134b9f101d36be9
Ruud Diterwich <ruud@diterwich.com>
2014-05-18 13:33:55 +0200

    Publishing info

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-json-pull-parser
+json-parser
 ================
 
 JSON parser with the following aims:
-- Provide pull interface
 - As fast as possible
 - Support multiple JSON AST's
+- Provide streaming interface
 
-{{{
+```scala
 	"org.scalastuff" %% "json-pull-parser" % "1.0.0"
-}}}
+```
+
+performance
+===========
+
+The parser is hand-crafted, resulting in very good performance. It's up to par with Jackson, more than 350 times faster than the default json parser (that shipped with scala until 2.10), and about 25 times faster than the Spray parser.
--- project/Build.scala
+++ project/Build.scala
@@ -3,18 +3,17 @@ import Keys._
 import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
 import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseCreateSrc
 
-object JsonPullParserBuild extends Build {
+object JsonPullBuild extends Build {
 
   def defaultSettings =
     Project.defaultSettings ++
       Seq(
         sbtPlugin := false, 
-        organization := "net.scalaleafs",
+        organization := "org.scalastuff",
         version := "1.0.0-SNAPSHOT",
         scalaVersion := "2.10.4",
         crossScalaVersions := Seq("2.10.4", "2.11.0"),
         scalacOptions ++= Seq("-deprecation", "-unchecked", "-encoding", "utf8", "-feature"),
-        //scalacOptions ++= Seq("-language:implicitConversions", "-language:postfixOps", "-language:reflectiveCall", "-language:higherKinds", "-language:existentials", "-language:reflectiveCalls"),
         EclipseKeys.createSrc := EclipseCreateSrc.Default + EclipseCreateSrc.Resource,
         EclipseKeys.withSource := true)
           
@@ -23,7 +22,7 @@ object JsonPullParserBuild extends Build {
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => false },
     licenses := Seq("The Apache Software Licence, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
-    homepage := Some(url("http://scalaleafs.net")),
+    homepage := Some(url("http://scalastuff.org")),
     publishTo := {
       if (version.value.trim.endsWith("SNAPSHOT"))
         Some("snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
@@ -31,8 +30,8 @@ object JsonPullParserBuild extends Build {
         Some("releases"  at "https://oss.sonatype.org/service/local/staging/deploy/maven2")},
     pomExtra := 
       <scm>
-        <connection>scm:git:git@github.com:scalastuff/scalaleafs.git</connection>
-        <url>https://github.com/scalastuff/scalaleafs</url>
+        <connection>scm:git:git@github.com:scalastuff/json-parser.git</connection>
+        <url>https://github.com/scalastuff/json-parser</url>
       </scm>
       <developers>
         <developer>
@@ -43,7 +42,7 @@ object JsonPullParserBuild extends Build {
       </developers>) 
 
     
-  val jsonPullParser = Project(id = "json-pull-parser", base = file("."), settings = defaultSettings ++ publishSettings ++ Seq(
+  val jsonParser = Project(id = "json-parser", base = file("."), settings = defaultSettings ++ publishSettings ++ Seq(
     libraryDependencies += "io.spray" %% "spray-json" % "1.2.6" % "optional" withSources(),
     libraryDependencies += "org.specs2" %% "specs2" % "2.3.12" % "test" withSources(),
     resolvers += "spray repo" at "http://repo.spray.io"))
